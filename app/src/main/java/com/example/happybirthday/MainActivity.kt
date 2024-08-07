e059666d807b83b77dde9169aba949c68ab35ac5
riyousya <riyousya@DESKTOP-4PI3E2I>
2024-08-07 13:27:38 +0900

    GreetingImageからGreetingText他を呼び出すように変更

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.happybirthday
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,10 +19,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthDayTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,13 +66,34 @@ fun GreetingText(message:String,
     }
 }
 
+@Composable
+fun GreetingImage(message:String,from: String,modifier: Modifier=Modifier){
+    val image = painterResource(id = R.drawable.androidparty)
+    Box(Modifier){
+        Image(
+            painter = image,
+            contentDescription =  null
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
 @Preview(
     showBackground = true,
     showSystemUi = false,
 )
+
 @Composable
 fun BirthDayCardPreview() {
     HappyBirthDayTheme {
-        GreetingText(message = "Happy Birthday Sam!" ,from = "From Taro")
+        GreetingImage(
+            message = "Happy Birthday Sam!" ,
+            from = "From Taro")
     }
 }
